013aa243a1007ff9c5c5713e36b93d1538c20c55
deadpoolfan80085 <manbatsr@gmail.com>
2021-01-26 20:39:54 -0500

    chain file added

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint
+var log6;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var chain
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -15,7 +16,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+log6 = new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -27,6 +28,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
+    
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -35,6 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    chain = new Chain(bird.body,log6.body)
+  
 
 }
 
@@ -61,4 +65,6 @@ function draw(){
 
     bird.display();
     platform.display();
-}
+    log6.display();
+    chain.display();
+   }
